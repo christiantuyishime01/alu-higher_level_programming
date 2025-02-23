a3ad6b9566f9d5b2e41df021e3f6a147f1b9ff89
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-02-23 08:43:20 -0800

    This is my answer

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -14,4 +14,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple_b[:2]
     first_element = tuple_a[0] + tuple_b[0]
     second_element = tuple_a[1] + tuple_b[1]
-    return tuple((first_element, second_element))`
+    return tuple((first_element, second_element))
