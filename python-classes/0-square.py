8da2b6144905b5d0a12dc3e22e2042306959aa26
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-05 08:29:35 -0800

    This is my answer

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Insert an empty class Square, It will define a square"""
+
+class Square:
+    """This class is empty, It defines a square"""
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+This README.md file belongs to python-classes
