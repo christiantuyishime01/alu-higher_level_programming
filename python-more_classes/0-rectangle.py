661599795185cf380f3b3ffc35acf45e064758ea
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-05 09:04:17 -0800

    This is my answer

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write an empty class Rectangle which will define a rectangle"""
+
+
+class Rectangle:
+    """Write an empty class Rectangle which will define a rectangle"""
+    pass
