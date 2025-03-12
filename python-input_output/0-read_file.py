d99d59292acbee50c05f4beee6a2084d7aaa6fd5
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-12 02:07:11 -0700

    This is my answer

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""It defines a text file reading function."""
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+    """It prints the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(),end="")
 
