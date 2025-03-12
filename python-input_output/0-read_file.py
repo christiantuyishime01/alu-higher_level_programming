5be40430fbd66b066c036202394c01ae55e2d0c4
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-12 02:18:33 -0700

    This is my answer

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
     """It prints the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
 
