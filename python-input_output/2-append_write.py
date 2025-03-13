70e95a0b0c1a9ece472b531626b5c7a61a18f376
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-13 00:44:36 -0700

    This is my answer

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
+# Open the file in append mode ("a"), with UTF-8 encoding
 def append_write(filename="", text=""):
-    # Open the file in append mode ("a"), with UTF-8 encoding
     # "a" mode ensures the file is created if it doesn't already exist
     with open(filename, "a", encoding="utf-8") as file:
         # Write the text to the end of the file
