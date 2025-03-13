eba428aeb6a5f6d7277395e27fcd31d56e0a76ad
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-13 00:36:53 -0700

    This is my answer

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""It defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
+    """Writing a string to the UTF8 text file.
+    Args:
+        filename (str): Name the file.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
 
