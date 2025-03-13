39c5796179e1ff2dafd917bb2fcf2008ebb430f8
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-13 14:19:39 -0700

    This is my answer

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write('The only way to do great work is to love what you do - Steve Jobs, 2015-10-19\n')
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 exit(1)
