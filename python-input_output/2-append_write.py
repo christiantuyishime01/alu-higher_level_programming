cc82019a3cf360641fefdc7912658566077b6dec
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-03-13 00:54:36 -0700

    This is my answer

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""It defines a file-appending function."""
 
 
-# Open the file in append mode ("a"), with UTF-8 encoding
 def append_write(filename="", text=""):
-    # "a" mode ensures the file is created if it doesn't already exist
-    with open(filename, "a", encoding="utf-8") as file:
-        # Write the text to the end of the file
-        # file.write() returns the number of characters written
-        return file.write(text)
+    """It appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
